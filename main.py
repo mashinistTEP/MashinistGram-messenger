e409b0dbd433f022461eb3476070e868e8c1c46d
mashinistTEP <kirillfillpromortis@gmail.com>
2026-05-14 13:39:47 +0300

    Allow cleartext traffic

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ class API:
 class LoginScreen(Screen):
     def __init__(self, **kw):
         super().__init__(**kw)
-        l = BoxLayout(orientation='vertical', padding=[40, 200, 40, 200], spacing=15)
+        l = BoxLayout(orientation='vertical', padding=[40, 120, 40, 280], spacing=15)
         l.add_widget(Label(text='MashinistGram', font_size=32, color=PURPLE, bold=True, size_hint_y=None, height=80))
         self.email = TextInput(hint_text='Email', multiline=False, size_hint_y=None, height=50)
         self.pw = TextInput(hint_text='Пароль', password=True, multiline=False, size_hint_y=None, height=50)
@@ -57,7 +57,7 @@ class LoginScreen(Screen):
 class RegisterScreen(Screen):
     def __init__(self, **kw):
         super().__init__(**kw)
-        l = BoxLayout(orientation='vertical', padding=[40, 100, 40, 100], spacing=15)
+        l = BoxLayout(orientation='vertical', padding=[40, 60, 40, 220], spacing=12)
         l.add_widget(Label(text='Регистрация', font_size=28, color=PURPLE, bold=True, size_hint_y=None, height=60))
         self.fn = TextInput(hint_text='Имя', multiline=False, size_hint_y=None, height=50)
         self.ln = TextInput(hint_text='Фамилия', multiline=False, size_hint_y=None, height=50)
